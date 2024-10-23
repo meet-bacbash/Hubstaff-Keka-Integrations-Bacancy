72fa5938ccf0b491ca4859b9da1a9506bd38a6e9
Manthan Soni <manthan.soni@bacancy.com>
2024-10-23 15:04:55 +0530

    create time bug fixed commit

--- hubstaff_data.py
+++ hubstaff_data.py
@@ -36,11 +36,11 @@ def hubstaff_main(q1,logger):
     todays_date = date.today()
     yesterdays_date = todays_date - timedelta(days=1)
 
-    # start_date = f'{yesterdays_date}T00:00:00Z'
-    # end_date = f'{yesterdays_date}T23:59:00Z'
+    start_date = f'{yesterdays_date}T00:00:00Z'
+    end_date = f'{yesterdays_date}T23:59:00Z'
 
-    start_date = '2024-10-21T00:00:00Z'
-    end_date = '2024-10-21T23:59:00Z'
+    # start_date = '2024-10-21T00:00:00Z'
+    # end_date = '2024-10-21T23:59:00Z'
 
     url = f'https://api.hubstaff.com/v2/organizations/{bac_org_id}/activities'
 
