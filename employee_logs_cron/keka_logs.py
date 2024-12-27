2a1fa9f2823c2930275dab0146b849c9d30e167a
Manthan Soni <manthan.soni@bacancy.com>
2024-12-27 15:11:21 +0530

    Features merge commit

--- employee_logs_cron/keka_logs.py
+++ employee_logs_cron/keka_logs.py
@@ -11,7 +11,7 @@ def keka_main(logger,q1):
 
     """
     l1 = {}
-    # url = "https://cin02.a.keka.com/v1/logs"
+    url = "https://cin02.a.keka.com/v1/logs"
 
     while not q1.empty():
         l1 = q1.get()
@@ -19,57 +19,56 @@ def keka_main(logger,q1):
     if l1:
         with tqdm(total=len(l1), desc="Uploading Data to Keka", unit="chunk") as pbar:
             for key,value in l1.items():
-                print(f"{key} :: {value}")
-                # emp_data = []
-                # for i in value:
-                #     val = {
-                #         "DeviceIdentifier": "648f6f6a-1edb-42fa-9f4a-3afaf254afdd",
-                #         "EmployeeAttendanceNumber": key,
-                #         "Timestamp": i['clock_in'],
-                #         "Status": 0
-                #     }
-                #
-                #     val2 = {
-                #         "DeviceIdentifier": "648f6f6a-1edb-42fa-9f4a-3afaf254afdd",
-                #         "EmployeeAttendanceNumber": key,
-                #         "Timestamp": i['clock_out'],
-                #         "Status": 1
-                #     }
-                #
-                #     emp_data.append(val)
-                #     emp_data.append(val2)
-                #
-                # payload = f"""
-                # [
-                #     {', '.join([f'''
-                #     {{
-                #         "DeviceIdentifier": "{entry['DeviceIdentifier']}",
-                #         "EmployeeAttendanceNumber": "{key}",
-                #         "Timestamp": "{entry['Timestamp']}",
-                #         "Status": {entry['Status']}
-                #     }}''' for entry in emp_data])}
-                # ]
-                # """
-                #
-                # headers = {
-                #     'Content-Type': 'application/json',
-                #     'X-API-Key': '29096d92-5808-4940-9ce0-f6ecbc305860'
-                # }
-                #
-                # response = requests.request("POST", url, headers=headers, data=payload, timeout=None)
-                #
-                # if response.status_code == 200:
-                #     print(response.text)
-                #     logger.info(f"{response.status_code} - {response.text} : payload - {payload}")
-                # else:
-                #     logger.error(f"{response.status_code} - {response.text} : payload - {payload}")
-                #
-                #     value['status_code'] = response.status_code
-                #     value['status_text'] = response.text
-                #     q1.put(value)
-                #     print(f"Error: {response.status_code}, {response.text}")
-                #
-                # # print(payload)
-                # # print("==============================================")
+                emp_data = []
+                for i in value:
+                    val = {
+                        "DeviceIdentifier": "648f6f6a-1edb-42fa-9f4a-3afaf254afdd",
+                        "EmployeeAttendanceNumber": key,
+                        "Timestamp": i['clock_in'],
+                        "Status": 0
+                    }
+
+                    val2 = {
+                        "DeviceIdentifier": "648f6f6a-1edb-42fa-9f4a-3afaf254afdd",
+                        "EmployeeAttendanceNumber": key,
+                        "Timestamp": i['clock_out'],
+                        "Status": 1
+                    }
+
+                    emp_data.append(val)
+                    emp_data.append(val2)
+
+                payload = f"""
+                [
+                    {', '.join([f'''
+                    {{
+                        "DeviceIdentifier": "{entry['DeviceIdentifier']}",
+                        "EmployeeAttendanceNumber": "{key}",
+                        "Timestamp": "{entry['Timestamp']}",
+                        "Status": {entry['Status']}
+                    }}''' for entry in emp_data])}
+                ]
+                """
+
+                headers = {
+                    'Content-Type': 'application/json',
+                    'X-API-Key': '29096d92-5808-4940-9ce0-f6ecbc305860'
+                }
+
+                response = requests.request("POST", url, headers=headers, data=payload, timeout=None)
+
+                if response.status_code == 200:
+                    print(response.text)
+                    logger.info(f"{response.status_code} - {response.text} : payload - {payload}")
+                else:
+                    logger.error(f"{response.status_code} - {response.text} : payload - {payload}")
+
+                    value['status_code'] = response.status_code
+                    value['status_text'] = response.text
+                    q1.put(value)
+                    print(f"Error: {response.status_code}, {response.text}")
+
+                # print(payload)
+                # print("==============================================")
                 pbar.update(1)
 
